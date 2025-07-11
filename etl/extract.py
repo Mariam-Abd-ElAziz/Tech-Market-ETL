653ce46498371f1a4abd2f6aa3d5b6c8a682173f
Mariam Essam <mariam.non30@gmail.com>
2025-07-11 08:09:39 +0300

    feat(extract_data):Added incremntal load logic.

--- etl/extract.py
+++ etl/extract.py
@@ -1,11 +1,26 @@
 import pandas as pd
 import os
+import pickle
 
-def extract_csv(data_directory):
+def extract_csv(data_directory,state_file="csv_file_state.pkl"):
+    if os.path.exists(state_file):
+        with open(state_file, 'rb') as f:
+            previous_state = pickle.load(f)
+    else:
+        previous_state = {}
+
+    current_state = {}
     csv_data = {}
+
     for filename in os.listdir(data_directory):
         if filename.endswith('.csv'):
             full_path = os.path.join(data_directory, filename)
-            csv_data[filename.replace('.csv', '')] = pd.read_csv(full_path)
-    return csv_data
+            last_updated_time = os.path.getmtime(full_path)
+            current_state[filename] = last_updated_time
+            # Check if file is new or modified
+            if filename not in previous_state or previous_state[filename] != last_updated_time:
+                csv_data[filename.replace('.csv', '')] = pd.read_csv(full_path)
+
+    return csv_data,current_state
+
 
