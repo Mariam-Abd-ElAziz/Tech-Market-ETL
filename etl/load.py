de702dbfe9cba87ea55fe2cfd0d7a5e3f9f87662
Mariam Essam <mariam.non30@gmail.com>
2025-07-11 23:05:49 +0300

    fix(load_data): added columns to solve surrogate key problem.

--- etl/load.py
+++ etl/load.py
@@ -3,23 +3,22 @@ import psycopg2
 import pandas as pd
 from config import DB_CONFIG
 import logging
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(_name_)
 
-def load_df_to_staging(df:pd.DataFrame, table_name:str):
+def load_df_to_table(df:pd.DataFrame, table_name:str,schema_name:str="public"):
     try:
         tmp = StringIO()
         df.to_csv(tmp, index=False, header=True)
         tmp.seek(0)
+        columns = ', '.join(df.columns)
+
         with psycopg2.connect(**DB_CONFIG) as conn:
             with conn.cursor() as cursor:
                     copy_sql = f"""
-                            COPY Staging.{table_name}
+                            COPY {schema_name}.{table_name}  ({columns})
                             FROM STDIN WITH CSV HEADER
                         """
                     cursor.copy_expert(copy_sql, tmp)
 
     except Exception as e:
-        logging.error(f"Failed to load CSV into staging.{table_name}: {e}")
-
-
-
+        logging.error(f"Failed to load Data into {schema_name}.{table_name}:{e}")
